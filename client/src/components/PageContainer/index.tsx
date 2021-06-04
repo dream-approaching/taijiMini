37b8dc76662e3b044e1ea06208c9b671286ff0b3
zhengjinshou <zhengjinshou@tenwit.com>
2021-06-04 12:44:11 +0800

    提取公共组件

---
+++ client/src/components/PageContainer/index.tsx
@@ -0,0 +1,106 @@
+import Taro from '@tarojs/taro';
+import React from 'react';
+import { AtTabs, AtTabsPane } from 'taro-ui';
+import { View, Image, Text, ScrollView } from '@tarojs/components';
+import CommonVideo from '@src/components/CommonVideo';
+import { getFileCloudId } from '@src/utils';
+import styles from './index.module.less';
+
+class ImgItem {
+  Key: string;
+  ETag: string;
+  title?: string;
+}
+
+class VideoItem {
+  Key: string;
+  ETag: string;
+  title: string;
+  shotInfo?: string;
+}
+
+interface PropsType {
+  imgList: Array<ImgItem>;
+  videoList: Array<VideoItem>;
+  imageDataConfig: object;
+}
+interface MyState {
+  currentTab: number;
+}
+export default class extends React.Component<PropsType, MyState> {
+  state: MyState = {
+    currentTab: 0,
+  };
+
+  async componentDidMount() {}
+
+  handlePreviewImg = (currentImg) => {
+    const { imgList } = this.props;
+    Taro.previewImage({
+      current: currentImg, // 当前显示图片的http链接
+      urls: imgList.map((item) => getFileCloudId(item.Key)), // 需要预览的图片http链接列表
+    });
+  };
+
+  handleToggleTab = (value) => {
+    this.setState({
+      currentTab: value,
+    });
+  };
+
+  render() {
+    const { currentTab } = this.state;
+    const { imgList, videoList, imageDataConfig } = this.props;
+    const tabList = [{ title: '图文描述' }, { title: '视频描述' }];
+    return (
+      <View className={styles.page}>
+        <AtTabs
+          className={styles.tabs}
+          current={currentTab}
+          tabList={tabList}
+          onClick={this.handleToggleTab}
+        >
+          <AtTabsPane current={currentTab} index={0}>
+            <ScrollView scroll-y className={styles.orderList} lowerThreshold={200}>
+              <View className={styles.imgCon}>
+                {imgList.map((item, index) => {
+                  return (
+                    <View key={item.ETag}>
+                      <View className={styles.imgTitle}>
+                        <Text className={styles.titleText}>{`${index + 1}、${
+                          imageDataConfig[item.Key].title
+                        }`}</Text>
+                      </View>
+                      <Image
+                        onClick={() => this.handlePreviewImg(getFileCloudId(item.Key))}
+                        mode='widthFix'
+                        lazyLoad
+                        className={styles.img}
+                        src={getFileCloudId(item.Key)}
+                      />
+                    </View>
+                  );
+                })}
+              </View>
+            </ScrollView>
+          </AtTabsPane>
+          <AtTabsPane current={currentTab} index={1}>
+            <ScrollView scroll-y className={styles.orderList} lowerThreshold={200}>
+              {videoList.map((item) => {
+                return (
+                  <View className={styles.videoCon} key={item.ETag}>
+                    <CommonVideo
+                      title={item.title}
+                      shotInfo={item.shotInfo}
+                      src={getFileCloudId(item.Key)}
+                    />
+                  </View>
+                );
+              })}
+            </ScrollView>
+          </AtTabsPane>
+        </AtTabs>
+      </View>
+    );
+  }
+}
--- client/src/pages/eighteen3/baiheliangchi/index.tsx
+++ client/src/pages/eighteen3/baiheliangchi/index.tsx
@@ -1,11 +1,8 @@
 import Taro from '@tarojs/taro';
 import React from 'react';
-import { View, Image, Text } from '@tarojs/components';
-import CommonTitle from '@src/components/commonTitle';
-import CommonVideo from '@src/components/CommonVideo';
-import { getFileCloudId, getVideoType } from '@src/utils';
+import { getVideoType } from '@src/utils';
 import { videoType } from '@src/config/constants';
-import styles from './index.module.less';
+import PageContainer from '@src/components/PageContainer';
 import { imageDataConfig, videoDataConfig } from './dataConfig';
 
 class ImgItem {
@@ -56,57 +53,10 @@ export default class extends React.Component<{}, MyState> {
     });
   }
 
-  handlePreviewImg = (currentImg) => {
-    const { imgList } = this.state;
-    Taro.previewImage({
-      current: currentImg, // 当前显示图片的http链接
-      urls: imgList.map((item) => getFileCloudId(item.Key)), // 需要预览的图片http链接列表
-    });
-  };
-
   render() {
     const { imgList, videoList } = this.state;
-    console.log('%c zjs imgList:', 'color: #0e93e0;background: #aaefe5;', imgList);
-    // const parse = {};
-    // imgList.forEach((item) => (parse[item.Key] = { title: '' }));
-    // console.log('%c zjs json:', 'color: #0e93e0;background: #aaefe5;', JSON.stringify(parse));
     return (
-      <View className={styles.page}>
-        <CommonTitle level={1} title='图文描述' />
-        <View className={styles.imgCon}>
-          {imgList.map((item, index) => {
-            return (
-              <View key={item.ETag}>
-                <View className={styles.imgTitle}>
-                  <Text className={styles.titleText}>{`${index + 1}、${
-                    imageDataConfig[item.Key].title
-                  }`}</Text>
-                </View>
-                <Image
-                  onClick={() => this.handlePreviewImg(getFileCloudId(item.Key))}
-                  mode='widthFix'
-                  lazyLoad
-                  className={styles.img}
-                  src={getFileCloudId(item.Key)}
-                />
-              </View>
-            );
-          })}
-        </View>
-
-        <CommonTitle level={1} title='视频描述' />
-        {videoList.map((item) => {
-          return (
-            <View className={styles.videoCon} key={item.ETag}>
-              <CommonVideo
-                title={item.title}
-                shotInfo={item.shotInfo}
-                src={getFileCloudId(item.Key)}
-              />
-            </View>
-          );
-        })}
-      </View>
+      <PageContainer imgList={imgList} videoList={videoList} imageDataConfig={imageDataConfig} />
     );
   }
 }
