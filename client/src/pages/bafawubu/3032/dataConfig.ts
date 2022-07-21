1f5ac0232ab2a1b3a44696ff04ee7feb25d1f2df
zhengjinshou <zhengjinshou@qtrade.com.cn>
2022-07-21 13:40:03 +0800

    style: 八法五步

--- client/src/pages/bafawubu/3032/dataConfig.ts
+++ client/src/pages/bafawubu/3032/dataConfig.ts
@@ -1,19 +1,24 @@
 export const imageDataConfig = {
-  'bafawubu/111351.jpg': { desc: '重心右移 扣左脚' },
-  'bafawubu/111356.jpg': { desc: '收右脚（两手放在两胯外侧）' },
-  'bafawubu/111406.jpg': { desc: '提左脚，挑掌（肘膝相合）' },
-  'bafawubu/111408.jpg': { desc: '坐腕推掌' },
-  'bafawubu/111412.jpg': { desc: '落脚' },
-  'bafawubu/111414.jpg': { desc: '翻掌' },
-  'bafawubu/111420.jpg': { desc: '左手下採 右手上挑' },
-  'bafawubu/111423.jpg': { desc: '坐腕推掌' },
-  'bafawubu/111430.jpg': { desc: '落脚碾脚 两脚平行' },
-  'bafawubu/111443.jpg': { desc: '两腕交叉，掌心朝内，与肩同高' },
-  'bafawubu/111448.jpg': { desc: '翻掌' },
-  'bafawubu/111451.jpg': { desc: '分掌' },
-  'bafawubu/111456.jpg': { desc: '手落身起 落于大腿前' },
-  'bafawubu/111509.jpg': { desc: '手落于大腿外侧' },
-  'bafawubu/111517.jpg': { desc: '收脚并步还原' },
+  // 第 30 势   中定左右独立
+  'bafawubu/111351.jpg': { desc: '1. 重心右移 扣左脚', block: '第 30 势   中定左右独立' },
+  'bafawubu/111356.jpg': { desc: '2. 收右脚（两手放在两胯外侧）' },
+  'bafawubu/111406.jpg': { desc: '3. 提左脚，挑掌（肘膝相合）' },
+  'bafawubu/111408.jpg': { desc: '4. 坐腕推掌' },
+  'bafawubu/111412.jpg': { desc: '5. 落脚' },
+  'bafawubu/111414.jpg': { desc: '6. 翻掌' },
+  'bafawubu/111420.jpg': { desc: '7. 左手下採 右手上挑' },
+  'bafawubu/111423.jpg': { desc: '8. 坐腕推掌' },
+
+  // 第 31 势   十字手
+  'bafawubu/111430.jpg': { desc: '1. 落脚碾脚 两脚平行', block: '第 31 势   十字手' },
+  'bafawubu/111443.jpg': { desc: '2. 两腕交叉，掌心朝内，与肩同高' },
+
+  // 第 32 势   收势
+  'bafawubu/111448.jpg': { desc: '1. 翻掌', block: '第 32 势   收势' },
+  'bafawubu/111451.jpg': { desc: '2. 分掌' },
+  'bafawubu/111456.jpg': { desc: '3. 手落身起 落于大腿前' },
+  'bafawubu/111509.jpg': { desc: '4. 手落于大腿外侧' },
+  'bafawubu/111517.jpg': { desc: '5. 收脚并步还原' },
 };
 
 export const videoDataConfig = {
