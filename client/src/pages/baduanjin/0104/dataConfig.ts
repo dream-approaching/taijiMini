58ff2f5a2c9960548a383cae028a9b19f45bc1a7
zhengjinshou <zhengjinshou@qtrade.com.cn>
2022-07-21 11:17:37 +0800

    style: 八段锦

--- client/src/pages/baduanjin/0104/dataConfig.ts
+++ client/src/pages/baduanjin/0104/dataConfig.ts
@@ -1,10 +1,13 @@
 export const imageDataConfig = {
+  // 预备势
   'baduanjin/103033.jpg': { desc: '1. 并步站立', block: '预备势' },
   'baduanjin/103034.jpg': { desc: '2. 左脚开立，与肩同宽' },
   'baduanjin/103035.jpg': { desc: '3. 两掌内旋，掌心向后' },
   'baduanjin/103036.jpg': { desc: '4. 两手抬起约 45°，与胯同高' },
   'baduanjin/103037.jpg': { desc: '5. 屈膝下抱' },
   'baduanjin/103038.jpg': { desc: '6. 深呼吸三次（吸气时收腹提肛，呼气时丹田前鼓，命门后撑）' },
+
+  // 第一段: 两手托天理三焦
   'baduanjin/103054.jpg': {
     desc: '1. 两手交叉（第一次要先把手落下来）',
     block: '第一段: 两手托天理三焦',
@@ -16,6 +19,8 @@ export const imageDataConfig = {
   'baduanjin/103136.jpg': { desc: '6. 手朝两侧打开（立掌，用力，感觉撑开两座山）' },
   'baduanjin/103143.jpg': { desc: '7. 手指伸直（用力，感觉双手插到山里面）' },
   'baduanjin/103150.jpg': { desc: '8. 屈膝下抱（放松，抱于腹前，掌心朝上）' },
+
+  // 第二段: 左右开弓似射雕
   'baduanjin/103211.jpg': {
     desc: '1. 重心右移，开左脚（右侧弓步，手不动）',
     block: '第二段: 左右开弓似射雕',
@@ -27,6 +32,8 @@ export const imageDataConfig = {
   'baduanjin/103239.jpg': { desc: '6. 右侧弓步（两手伸平）' },
   'baduanjin/103244.jpg': { desc: '7. 并步站立（做最后一个时，收回平行步站立）' },
   'baduanjin/103249.jpg': { desc: '8. 屈膝下抱（掌心朝上，最后一个时，平行步站立）' },
+
+  // 第三段: 调理脾胃须单举
   'baduanjin/103313.jpg': {
     desc: '1. 直立上托（左手上托，右手不动）',
     block: '第三段: 调理脾胃须单举',
@@ -39,6 +46,8 @@ export const imageDataConfig = {
   'baduanjin/103331.jpg': {
     desc: '5. 屈膝下抱（抱于腹前，掌心朝上。最后一个动作是两掌按于大腿前两侧，掌心朝下）',
   },
+
+  // 第四段: 五劳七伤往后瞧
   'baduanjin/103346.jpg': { desc: '1. 直立落掌', block: '第四段: 五劳七伤往后瞧' },
   'baduanjin/103351.jpg': { desc: '2. 左转摆头（用力，身体不要歪，尽量眼手头同时到）' },
   'baduanjin/103356.jpg': { desc: '3. 放松' },
